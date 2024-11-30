485127d7dc1a4442efea0412ee2f6f7a44cf0ff3
PresleyChew <E1115692@u.nus.edu>
2024-12-01 02:16:07 +0800

    Define skeletal functions for validity check

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-### cleanup
+#Test for input validity with respect to row, if for particular row specified
+#by row contains num, then return boolean false, else true.
+def isValidInputRow(arr, row, num) {
 
+}
+
+#Test for input validity with respect to col, if for particular column specified
+#by column contains num, then return boolean false, else true.
+def isValidInputColumn(arr, col, num) {
+
+}
+
+#Test for input validity with respect to the 3 by 3 grid it is in, if number specified
+#by num is within box containing the col and row, then return boolean false,
+#else true.
+def isValidInputBox(arr, col, row, num) {
+
+}
